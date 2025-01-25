6832bc1ab6731e312e2dcf81d695fa52766b6845
s29741-pj <s29741@pjwstk.edu.pl>
2025-01-25 15:46:07 +0100

    setup

---
+++ s29741Bank/src/test/java/com/example/s29741bank/S29741BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s29741bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S29741BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
