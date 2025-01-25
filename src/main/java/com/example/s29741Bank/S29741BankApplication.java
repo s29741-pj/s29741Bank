a543fa5a911a78ef8b3574390db7c52e40c83d98
s29741-pj <s29741@pjwstk.edu.pl>
2025-01-25 15:39:06 +0100

    initial commit

---
+++ src/main/java/com/example/s29741Bank/S29741BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s29741Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29741BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S29741BankApplication.class, args);
+	}
+
+}
