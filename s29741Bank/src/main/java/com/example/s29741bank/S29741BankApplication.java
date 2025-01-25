b2b39e16d5a00a3d859c93f911f23191c341b9a4
s29741-pj <s29741@pjwstk.edu.pl>
2025-01-25 17:15:24 +0100

    new class

--- s29741Bank/src/main/java/com/example/s29741bank/S29741BankApplication.java
+++ s29741Bank/src/main/java/com/example/s29741bank/S29741BankApplication.java
@@ -6,6 +6,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class S29741BankApplication {
 
+    private final Account account;
+    private final AccountStorage accountStorage;
+    private final AccountService accountService;
+    private final Controller controller;
+    private final TransactionStatus transactionStatus;
+
+    public S29741BankApplication(Account account, AccountStorage accountStorage, AccountService accountService, Controller controller, TransactionStatus transactionStatus) {
+        this.account = account;
+        this.accountStorage = accountStorage;
+        this.accountService = accountService;
+        this.controller = controller;
+        this.transactionStatus = transactionStatus;
+    }
+
+public void run(){
+
+}
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(S29741BankApplication.class, args);
     }
